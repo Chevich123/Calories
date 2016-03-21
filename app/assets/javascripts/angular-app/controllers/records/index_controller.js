3db523bb96347301eb2b2be98cd246fc60b10afd
Andrew Rogachevich <andrey.rogachevich@azati.com>
2016-03-21 18:50:36 +0300

    angular-app: Manage Records (Filter partially)

--- app/assets/javascripts/angular-app/controllers/records/index_controller.js
+++ app/assets/javascripts/angular-app/controllers/records/index_controller.js
@@ -9,18 +9,34 @@ app.controller('RecordsIndexController', ['$scope', '$location', 'Record', funct
         if ($location.search().date_to) {
             $scope.filter.date_to = $location.search().date_to;
         }
+        if ($location.search().time_from) {
+            $scope.filter.time_from = $location.search().time_from;
+        }
+        if ($location.search().time_to) {
+            $scope.filter.time_to = $location.search().time_to;
+        }
+    };
+
+    var calculateCalories = function(){
+
     };
 
     $scope.load = function () {
         initFilter();
-        Record.query(function (data) {
+
+        Record.query($scope.filter, function (data) {
             var array = data;
             array.forEach(function(record) {
                 record.display_date = new Date(record.date)
             });
             $scope.records = array;
+            calculateCalories();
         });
 
+    };
+
+    $scope.filter_records = function(){
+        $location.search({date_from: $scope.filter.date_from, date_to: $scope.filter.date_to, time_from: $scope.filter.time_from, time_to: $scope.filter.time_to});
     };
 
     $scope.load();
